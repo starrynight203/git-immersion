9b38a93ddbb06b6d529cc1e8829d3106a4242d80
Hannah Smith <hannahsmith@Hannahs-MBP.attlocal.net>
2016-02-14 16:14:11 -0500

    Hello uses Greeter

--- hello.rb
+++ hello.rb
@@ -5,3 +5,10 @@ name = ARGV.first || "World"
 name = ARGV.first || "World"
 
 #random comment
+require 'greeter'
+
+# Default is World
+name = ARGV.first || "World"
+
+greeter = Greeter.new(name)
+puts greeter.greet
